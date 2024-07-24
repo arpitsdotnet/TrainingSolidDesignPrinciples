1f4473c33f1f75b2be2debba55070e4d8c1d29e2
Arpit Shrivastava <shrivastava.arpit@hotmail.com>
2024-07-24 17:55:19 +0530

    R. LSP : Solution Commit.

---
+++ TrainingSolidPrinciples/LSP/Solution/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TrainingSolidPrinciples.LSP.Solution;
+public class Program
+{
+    static void Main(string[] args)
+    {
+        IFruit apple = new Apple();
+        Console.WriteLine(apple.GetColor());
+
+        IFruit orange = new Orange();
+        Console.WriteLine(orange.GetColor());
+    }
+
+    public interface IFruit
+    {
+        string GetColor();
+    }
+
+    public class Apple : IFruit
+    {
+        public string GetColor()
+        {
+            return "Red";
+        }
+    }
+
+    public class Orange : IFruit
+    {
+        public string GetColor()
+        {
+            return "Orange";
+        }
+    }
+}
